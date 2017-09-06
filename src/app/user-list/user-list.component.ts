8f93e76278aa34fd752f0af0054fa8bc85528df2
Babu Aryal <baburam.aryal@davita.com>
2017-09-05 22:19:50 -0600

    added auth guard

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { UserService } from "../user.service";
-import { User } from "../model/user";
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {UserService} from "../user.service";
+import {User} from "../model/user";
 
 @Component({
   selector: 'app-user-list',
@@ -11,33 +11,34 @@ export class UserListComponent implements OnInit {
 
   @ViewChild('userDetailModel') userDetailModel;
 
-  users:Array<User> = [];
-  errorMsg:string = null;
+  users: Array<User> = [];
+  errorMsg: string = null;
 
-  selectedUser:User;
+  selectedUser: User;
 
-  constructor(private userService:UserService) { }
+  constructor(private userService: UserService) {
+  }
 
   ngOnInit() {
 
     this.userService.getAllUsers()
-    .subscribe(
-       data => {
-         this.users = data
-       }
-       ,
-       error => {
-         this.errorMsg = error.hasOwnProperty("message") ? <any>error["message"] : "Server Error. Please check your connection and try agian.";
-       }
-     );
+      .subscribe(
+        data => {
+          this.users = data
+        }
+        ,
+        error => {
+          this.errorMsg = error.hasOwnProperty("message") ? <any>error["message"] : "Server Error. Please check your connection and try agian.";
+        }
+      );
   }
 
-  openModel(user:User = null) {
+  openModel(user: User = null) {
     this.selectedUser = user;
     this.userDetailModel.nativeElement.className = 'modal fade show';
   }
 
   closeModel() {
     this.userDetailModel.nativeElement.className = 'modal hide';
- }
+  }
 }
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,74 +1,92 @@
-import { Injectable } from '@angular/core';
-import { Router } from "@angular/router";
-import { Http, Headers, RequestOptions } from "@angular/http";
+import {Injectable} from '@angular/core';
+import {Router} from "@angular/router";
+import {Http, Headers, RequestOptions} from "@angular/http";
 
-import { Observable } from 'rxjs/Observable';
+import {Observable} from 'rxjs/Observable';
 import {Observer} from 'rxjs/Observer';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
 
-import { User } from "./model/user";
+import {User} from "./model/user";
 
 @Injectable()
 export class UserService {
 
   link = ['/user-list'];
 
-  private userUrl:string ='https://microservice-api.herokuapp.com/';
+  private userUrl: string = 'https://microservice-api.herokuapp.com/';
 
-  userEndpoint:string = "https://microservice-api.herokuapp.com/";
-  LOGIN_PATH:string = "login";//for login
-  GET_ALL_USERS_PATH:string = "users";//to load all users from database
-  ADD_USER_PATH:string = "user/add";//to add new user
+  userEndpoint: string = "https://microservice-api.herokuapp.com/";
+  LOGIN_PATH: string = "login";//for login
+  GET_ALL_USERS_PATH: string = "users";//to load all users from database
+  ADD_USER_PATH: string = "user/add";//to add new user
 
-  constructor(private router: Router, private http:Http) { }
+  loggedIn: boolean;
+  loggedInUser: User;
 
-  login(email:string, password:string):Observable<User[]> {
-    console.log(this.userEndpoint + this.LOGIN_PATH + "?email=" + email +"&password=" + password);
-                         
-    return this.http.get(this.userEndpoint + this.LOGIN_PATH + "?email=" + email +"&password=" + password)
-                    .map(response => {
-                      this.router.navigate(this.link);
-                      return response.json();
-                    })
-                    .catch(error => {
-                      return Observable.throw(error.json());
-                  });
+  constructor(private router: Router, private http: Http) {
   }
 
-  getAllUsers():Observable<User[]> {
+  isLoggedIn() {
+    return this.loggedIn;
+  }
+
+  getLoggedInUser() {
+    return this.loggedInUser;
+  }
+
+  getLoginUrl(): string {
+    return "/login";
+  }
+
+  login(email: string, password: string): Observable<User[]> {
+    console.log(this.userEndpoint + this.LOGIN_PATH + "?email=" + email + "&password=" + password);
+
+    return this.http.get(this.userEndpoint + this.LOGIN_PATH + "?email=" + email + "&password=" + password)
+      .map(response => {
+        this.loggedIn = true;
+        this.router.navigate(this.link);
+        this.loggedInUser = response.json();
+        return response.json();
+      })
+      .catch(error => {
+        return Observable.throw(error.json());
+      });
+  }
+
+  getAllUsers(): Observable<User[]> {
 
     return this.http.get(this.userEndpoint + this.GET_ALL_USERS_PATH)
-                    .map(response=> {
-                      return response.json();
-                    })
-                    .catch(error => {
-                      console.log(error.json());
-                      return Observable.throw(error.json());
-                  });
+      .map(response => {
+        return response.json();
+      })
+      .catch(error => {
+        console.log(error.json());
+        return Observable.throw(error.json());
+      });
   }
 
-  saveUser(user:User):Observable<any> {
-    
+  saveUser(user: User): Observable<any> {
+
     //let headers = new Headers();
     //headers.append('Content-Type', 'application/x-www-form-urlencoded');
 
-    let headers = new Headers({ 'Content-Type': 'application/json' });
-    let options = new RequestOptions({ headers: headers });
+    let headers = new Headers({'Content-Type': 'application/json'});
+    let options = new RequestOptions({headers: headers});
 
     console.log(user);
     console.log(JSON.stringify(user));
-    
+
     return this.http.post(this.userEndpoint + this.ADD_USER_PATH, JSON.stringify(user), options)
-                    .map(response => {
-                      this.router.navigate(this.link);
-                      return response.json();
-                    })
-                    .catch(error => {
-                      console.log(error.json());
-                      return Observable.throw(error.json());
-                  });
+      .map(response => {
+        this.router.navigate(this.link);
+        return response.json();
+      })
+      .catch(error => {
+        console.log(error.json());
+        return Observable.throw(error.json());
+      });
   }
 }
 
